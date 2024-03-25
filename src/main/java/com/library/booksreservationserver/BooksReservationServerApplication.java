e1a4b7b3bbdec24476346560ff015e4f2b172b87
bogdankis <chismbogdan@gmail.com>
2024-03-25 12:10:24 +0200

    first commit

---
+++ src/main/java/com/library/booksreservationserver/BooksReservationServerApplication.java
@@ -0,0 +1,13 @@
+package com.library.booksreservationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksReservationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksReservationServerApplication.class, args);
+	}
+
+}
