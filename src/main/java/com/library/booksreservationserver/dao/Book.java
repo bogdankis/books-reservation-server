86a048f412ce01ddbfd596dec495314bb23ac76e
bogdankis <chismbogdan@gmail.com>
2024-04-02 14:40:34 +0300

    added book class

---
+++ src/main/java/com/library/booksreservationserver/dao/Book.java
@@ -0,0 +1,32 @@
+package com.library.booksreservationserver.dao;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "book")
+@Data
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "copies_available")
+    private int copiesAvaiable;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "img_path")
+    private String img;
+
+
+}
