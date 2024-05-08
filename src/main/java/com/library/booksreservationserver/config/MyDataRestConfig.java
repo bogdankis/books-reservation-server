2ae6009e12080a00736636320ae484cd0b712845
bogdankis <chismbogdan@gmail.com>
2024-05-08 15:04:38 +0300

    made REST API methods post,patch,delete,put readonly

--- src/main/java/com/library/booksreservationserver/config/MyDataRestConfig.java
+++ src/main/java/com/library/booksreservationserver/config/MyDataRestConfig.java
@@ -1,4 +1,42 @@
 package com.library.booksreservationserver.config;
+import com.library.booksreservationserver.entity.Book;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
-public class MyDataRestConfig {
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer{
+    private String theAllowedOrigins = "http://localhost:3000";
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors){
+        HttpMethod[] theUnavailableActions = {HttpMethod.POST,
+                HttpMethod.PATCH,
+                HttpMethod.DELETE,
+                HttpMethod.PUT};
+        config.exposeIdsFor(Book.class);
+
+        disableHttpMethods(Book.class, config, theUnavailableActions);
+
+        /* Configure CORS Mapping */
+        cors.addMapping(config.getBasePath() + "/**")
+                .allowedOrigins(theAllowedOrigins);
+    }
+
+
+    private void disableHttpMethods(Class TheClass,
+                                    RepositoryRestConfiguration config,
+                                    HttpMethod[] theUnavailableActions){
+
+        config.getExposureConfiguration().
+                forDomainType(TheClass).
+                withItemExposure((metdata, httpMethods) ->
+                        httpMethods.disable(theUnavailableActions))
+                .withCollectionExposure((metdata, httpMethods) ->
+                        httpMethods.disable(theUnavailableActions));
+
+
+    }
 }
